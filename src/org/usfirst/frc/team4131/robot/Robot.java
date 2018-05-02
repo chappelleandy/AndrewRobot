9139184542bd66cdaa9be0f468fe34d6dd42a4ce
Andrew <chappelleandy@aol.com>
2018-05-01 19:01:17 -0700

    working arcade drive

--- src/org/usfirst/frc/team4131/robot/Robot.java
+++ src/org/usfirst/frc/team4131/robot/Robot.java
@@ -7,9 +7,21 @@
 
 package org.usfirst.frc.team4131.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 //test
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,11 +31,32 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends IterativeRobot {
+	//private static final Compressor compressor = new Compressor(61);
 	private static final String kDefaultAuto = "Default";
 	private static final String kCustomAuto = "My Auto";
 	private String m_autoSelected;
 	private SendableChooser<String> m_chooser = new SendableChooser<>();
 
+	//Variables for Talons y Xbox controller
+	public static final XboxController Controller = new XboxController(0);
+	
+	public static final TalonSRX talon5 = new TalonSRX(5);
+	public static final TalonSRX talon6 = new TalonSRX(6);
+	public static final TalonSRX talon7 = new TalonSRX(7);
+	
+	public static final Timer Timer = new Timer();
+	
+	public static final WPI_TalonSRX Right1 = new WPI_TalonSRX(1);
+	public static final WPI_TalonSRX Right2 = new WPI_TalonSRX(2);
+
+	SpeedControllerGroup Right = new SpeedControllerGroup(Right1, Right2);
+	
+	public static final WPI_TalonSRX Left1 = new WPI_TalonSRX(3);
+	public static final WPI_TalonSRX Left2 = new WPI_TalonSRX(4);
+	
+	SpeedControllerGroup Left = new SpeedControllerGroup(Left1, Left2);
+	
+	DifferentialDrive myDrive = new DifferentialDrive(Left, Right);
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -33,6 +66,7 @@ public class Robot extends IterativeRobot {
 		m_chooser.addDefault("Default Auto", kDefaultAuto);
 		m_chooser.addObject("My Auto", kCustomAuto);
 		SmartDashboard.putData("Auto choices", m_chooser);
+		
 	}
 
 	/**
@@ -75,7 +109,16 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		while (isOperatorControl() && isEnabled()) {
+			
+			Hand lefthand = GenericHID.Hand.kLeft;
+			Hand righthand = GenericHID.Hand.kRight;
+			myDrive.arcadeDrive(Controller.getY(lefthand), Controller.getX(righthand), false);
+    		
+    	}
 	}
+
+	
 
 	/**
 	 * This function is called periodically during test mode.
