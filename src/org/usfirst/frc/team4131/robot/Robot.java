5eaa5ebc698c4ee239e4aa86c2f92038d5a432dc
Andrew <chappelleandy@aol.com>
2018-05-03 18:07:21 -0700

    Gregor Code

--- src/org/usfirst/frc/team4131/robot/Robot.java
+++ src/org/usfirst/frc/team4131/robot/Robot.java
@@ -1,129 +1,67 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team4131.robot;
 
-import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-//test
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
+
 public class Robot extends IterativeRobot {
-	//private static final Compressor compressor = new Compressor(61);
-	private static final String kDefaultAuto = "Default";
-	private static final String kCustomAuto = "My Auto";
-	private String m_autoSelected;
-	private SendableChooser<String> m_chooser = new SendableChooser<>();
-
-	//Variables for Talons y Xbox controller
-	public static final XboxController Controller = new XboxController(0);
 	
-	public static final TalonSRX talon5 = new TalonSRX(5);
-	public static final TalonSRX talon6 = new TalonSRX(6);
-	public static final TalonSRX talon7 = new TalonSRX(7);
+	//Declaration of Variables:
 	
-	public static final Timer Timer = new Timer();
+	//Hands are for the Xbox controller, the HIDGeneric Class requires indication of left or right for certain things
+	Hand LeftHand = GenericHID.Hand.kLeft;
+	Hand RightHand = GenericHID.Hand.kRight;	
+	XboxController Controller = new XboxController(0);
 	
-	public static final WPI_TalonSRX Right1 = new WPI_TalonSRX(1);
-	public static final WPI_TalonSRX Right2 = new WPI_TalonSRX(2);
-
-	SpeedControllerGroup Right = new SpeedControllerGroup(Right1, Right2);
+	//Talons:
+	WPI_TalonSRX DriveLeft1 = new WPI_TalonSRX(0); 
+	WPI_TalonSRX DriveLeft2 = new WPI_TalonSRX(1);
+	WPI_TalonSRX DriveRight1 = new WPI_TalonSRX(2);
+	WPI_TalonSRX DriveRight2 = new WPI_TalonSRX(3);
+	WPI_TalonSRX Unused = new WPI_TalonSRX(4); //might be used later for something
+	WPI_TalonSRX Handler = new WPI_TalonSRX(5);
+	WPI_TalonSRX Arms = new WPI_TalonSRX(6);
+	WPI_TalonSRX ArmsRoller = new WPI_TalonSRX(7);
+	WPI_TalonSRX Launcher = new WPI_TalonSRX(8);
 	
-	public static final WPI_TalonSRX Left1 = new WPI_TalonSRX(3);
-	public static final WPI_TalonSRX Left2 = new WPI_TalonSRX(4);
+	//Brake mode and Coast mode
+	NeutralMode Brake = NeutralMode.Brake;
+	NeutralMode Coast = NeutralMode.Coast;
 	
-	SpeedControllerGroup Left = new SpeedControllerGroup(Left1, Left2);
+	//Groups of Talons for driving simplicity
+	SpeedControllerGroup Left = new SpeedControllerGroup(DriveLeft1, DriveLeft2);
+	SpeedControllerGroup Right = new SpeedControllerGroup(DriveRight1, DriveRight2);
 	
+	//Drive Base
 	DifferentialDrive myDrive = new DifferentialDrive(Left, Right);
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		m_chooser.addDefault("Default Auto", kDefaultAuto);
-		m_chooser.addObject("My Auto", kCustomAuto);
-		SmartDashboard.putData("Auto choices", m_chooser);
-		
-	}
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomousInit() {
-		m_autoSelected = m_chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + m_autoSelected);
-	}
-
-	/**
-	 * This function is called periodically during autonomous.
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (m_autoSelected) {
-			case kCustomAuto:
-				// Put custom auto code here
-				break;
-			case kDefaultAuto:
-			default:
-				// Put default auto code here
-				break;
-		}
-	}
-
-	/**
-	 * This function is called periodically during operator control.
-	 */
+	
+	//Encoders
+	Encoder ArmsEncoder = new Encoder(8,9); //DIO 8 and 9 for the Arms motor
+	Encoder ShooterEncoder = new Encoder(4,5); //DIO 4 and 5 for the Shooter motor
+	
+	//Limit Switches
+	DigitalInput ArmSwitchAbsoluteStop = new DigitalInput(7);
+	DigitalInput HandlerSwitch = new DigitalInput(6);
+	
 	@Override
 	public void teleopPeriodic() {
-		while (isOperatorControl() && isEnabled()) {
-			
-			Hand lefthand = GenericHID.Hand.kLeft;
-			Hand righthand = GenericHID.Hand.kRight;
-			myDrive.arcadeDrive(Controller.getY(lefthand), Controller.getX(righthand), false);
-    		
-    	}
+		//Drive Function, takes controller input and outputs to Drive
+		myDrive.arcadeDrive(Controller.getY(LeftHand), Controller.getX(RightHand), false);
+    	
+		//Stow Arms
+		//if(Controller.getXButtonPressed() && )
+    	System.out.println("ArmsEncoder: " + ArmsEncoder.get());
+    	
+    	Arms.setNeutralMode(Coast);
 	}
 
-	
-
-	/**
-	 * This function is called periodically during test mode.
-	 */
-	@Override
-	public void testPeriodic() {
-	}
 }
